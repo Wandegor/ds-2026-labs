3763c8fe88cefb310fc19207026c583bd11777e1
wander122156 <sov48656@mal.ru>
2026-03-04 16:06:40 +0300

    feat: исправил ошибки работы nginx, все заработало

--- Valuator/Program.cs
+++ Valuator/Program.cs
@@ -14,6 +14,9 @@ public class Program
         var redisConnectionString = builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379";
         var redis = ConnectionMultiplexer.Connect(redisConnectionString);
         
+        // Antiforgery токен будет хранится и доставаться не где то на диске, а в бд
+         // app1 генерит токен, сохраняет ключ в Redis (или использует уже существующий общий ключ).
+         // app2 проверяет токен, обращается к  Redis, находит нужный ключ и расшифровывает токен.
         builder.Services.AddDataProtection().PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
         builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
 
