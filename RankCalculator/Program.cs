492bcd878b57565d9a7866fe48c4ed261c57019d
wander122156 <sov48656@mail.ru>
2026-04-01 15:47:37 +0300

    feat: Сделал расчёт rank в RancCalculator

--- RankCalculator/Program.cs
+++ RankCalculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using StackExchange.Redis;
 
 namespace RankCalculator;
 
@@ -12,6 +13,10 @@ class Program
     {
         Console.WriteLine("Consumer started");
 
+        string redisHost = Environment.GetEnvironmentVariable("REDIS_HOST") ?? "localhost";
+        var redis = await ConnectionMultiplexer.ConnectAsync(redisHost);
+        IDatabase db = redis.GetDatabase();
+        
         ConnectionFactory factory = new ConnectionFactory
         {
             HostName = "localhost",
@@ -19,8 +24,9 @@ class Program
         await using IConnection connection = await factory.CreateConnectionAsync();
         await using IChannel channel = await connection.CreateChannelAsync();
 
-        await DeclareTopologyAsync(channel);
-        string consumerTag = await RunConsumer(channel);
+        await DeclareTopologyAsync(channel); // Создание очереди если нет
+        
+        string consumerTag = await RunConsumer(channel, db);
 
         Console.WriteLine("Press Enter to exit");
         Console.ReadLine();
@@ -30,23 +36,43 @@ class Program
         Console.WriteLine("done");
     }
 
-    private static async Task<string> RunConsumer(IChannel channel)
+    private static async Task<string> RunConsumer(IChannel channel, IDatabase db)
     {
         AsyncEventingBasicConsumer consumer = new(channel);
-        consumer.ReceivedAsync += (_, eventArgs) => ConsumeAsync(channel, eventArgs);
+        consumer.ReceivedAsync += (_, eventArgs) => ConsumeAsync(channel, eventArgs, db);
         return await channel.BasicConsumeAsync(
             queue: QueueName,
-            autoAck: false,
+            autoAck: false, // Ожидание личного подтверждения (Ack)
             consumer: consumer
         );
     }
 
-    private static async Task ConsumeAsync(IChannel channel, BasicDeliverEventArgs eventArgs)
+    private static async Task ConsumeAsync(IChannel channel, BasicDeliverEventArgs eventArgs, IDatabase db)
     {
-        Console.WriteLine("Consuming");
-        string message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
-        Console.WriteLine($"Consuming: {message} from subject {eventArgs.Exchange}");
-        await channel.BasicAckAsync(eventArgs.DeliveryTag, false);
+        string id = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+        Console.WriteLine($"Received task for ID: {id}");
+        
+        // pa3
+        string? text = await db.StringGetAsync($"TEXT-{id}");
+        
+        if (!string.IsNullOrEmpty(text))
+        {
+            double rank = 0;
+            foreach (char ch in text)
+            {
+                if (!char.IsLetter(ch)) rank++;
+            }
+            rank /= text.Length;
+
+            string rankString = Math.Round(rank, 4)
+                .ToString(System.Globalization.CultureInfo.InvariantCulture);
+
+            await db.StringSetAsync($"RANK-{id}", rankString);
+                
+            Console.WriteLine($"Calculated Rank: {rankString} for ID: {id}");
+        }
+        
+        await channel.BasicAckAsync(eventArgs.DeliveryTag, false); // Подтверждение, сообщ удаляется из очереди
     }
 
 
